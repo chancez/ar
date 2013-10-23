d975e701138799aa01e14c4bc079e82a85da2e10
Chance Zibolski <chance.zibolski@gmail.com>
2013-10-22 18:31:09 -0700

    Add getopt working example from gnu.org

---
+++ myar.c
@@ -0,0 +1,54 @@
+#include <ar.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int aflag = 0;
+    int bflag = 0;
+    char *cvalue = NULL;
+    int index;
+    int c;
+
+    int opterr = 0;
+
+    while ((c = getopt(argc, argv, "abc:")) != -1)
+        switch (c)
+        {
+        case 'a':
+            aflag = 1;
+            break;
+        case 'b':
+            bflag = 1;
+            break;
+        case 'c':
+            cvalue = optarg;
+            break;
+        case '?':
+            if (optopt == 'c')
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+            else if (isprint (optopt))
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf (stderr,
+                        "Unknown option character `\\x%x'.\n",
+                        optopt);
+            return 1;
+        default:
+            abort();
+        }
+
+    printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+            aflag, bflag, cvalue);
+
+    for (index = optind; index < argc; index++)
+        printf ("Non-option argument %s\n", argv[index]);
+    return 0;
+
+}
